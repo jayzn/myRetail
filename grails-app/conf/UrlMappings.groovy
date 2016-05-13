fe174c47584980965387aea4d75f495b8933d8bd
Jayanthi Narayanan <jayanthi.narayanan@gmail.com>
2016-05-12 23:54:12 -0500

    Formatted spacing

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -10,9 +10,9 @@ class UrlMappings {
         "/"(view:"/index")
         "500"(view:'/error')
 		
-		"/product/$id"(controller: "product", action: "show")
-		"/productList"(controller: "product", action: "list")
-		"/productList/$cat"(controller: "product", action: "list")
+	"/product/$id"(controller: "product", action: "show")
+	"/productList"(controller: "product", action: "list")
+	"/productList/$cat"(controller: "product", action: "list")
 		
 	}
 }
